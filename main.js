ca50098f7d089e87f34807ff9b5258cff49c8137
Richard5678 <frqrichard@gmail.com>
2021-12-23 18:48:35 +0800

    initial commit

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ for (var i = 0; i < 19; i++) {
     board[i] = new Array(19).fill(0);
 }
 
-//console.log(nj.array(board));
-
 var prev_move = new Array(2).fill(-1);
 var prev_ai_move = new Array(2).fill(-1);
 var prev_human_move = new Array(2).fill(-1);
@@ -59,7 +57,6 @@ let app = Vue.createApp({
             // if no stones have been placed on this spot
             if (this.board[i][j] == 0) {
                 this.board[i][j] = player;
-                //this.player = -this.player;
                 this.prev_move[0] = i;
                 this.prev_move[1] = j;
                 this.prev_human_move[0] = i;
@@ -74,8 +71,6 @@ let app = Vue.createApp({
                     this.draw = true;
                     this.game_ended = true;
                 }
-
-                //sleep(1000);
                 
                 setTimeout (() => {
                     this.ai_move(this.board);     
@@ -96,96 +91,16 @@ let app = Vue.createApp({
             }
             return b;
         },
-        /*
-        checkWinner(board, player, i, j, check) {
-            // horizontal
-            if (this.checkHorizontal(board, player, check)) {return true;}
-        
-            // vertical
-            //if (checkHorizontal(_.zip.apply(_, board), player)) {return;};
-            if (this.checkHorizontal(this.transpose(board), player, check)) { return true;};
-            
-            // diagonal \
-            if (this.checkDiagonal(board, 19, player, check)) {return true;};
-        
-            // diagonal /
-            var result = this.checkDiagonal(board.map(function(matrix){return matrix.reverse();}), 19, player, check);
-            board.map(function(matrix){return matrix.reverse();})
-            return result;
-        },
-        checkHorizontal(board, player) {
 
-            for (var i = 0; i < 19; i++) {
-                var count = 0;
-                for (var j = 0; j < 19; j++) {
-                    if (board[i][j] == player) {
-                        count++;
-                    } else {
-                        count = 0;
-                    }
-                    if (count >= 5) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        },
-        check_direction(row, col, dimension, board, player, check) {
-            var count = 0;
-            while (row < dimension) {
-                if (board[row][col] == player) {
-                    count++;
-                } else if (count >= 5) {
-                    //console.log(player, "won");
-                    //this.player_won = this.get_winner(this.player);
-                    //this.has_won = true;
-                    //this.game_ended = true;
-                    return true;
-                } else {
-                    count = 0;
-                }
-                row++;
-                col++;
-            }
-            return false;
-        },
-        checkDiagonal(board, dimension, player, check) {
-            // check all possibilities with starting box on the first column (j = 0)
-            var i = 0;
-            while (i <= dimension - 5) {
-                var row = i;
-                var col = 0;
-                if (this.check_direction(row, col, dimension, board, player, check)) {return true;};
-                i++;
-            }
-        
-            // check all possibilities with starting box on the first row (i = 0)
-            var j = 1;
-            while (j <= dimension - 5) {
-                var row = 0;
-                var col = j;
-                if (this.check_direction(row, col, dimension, board, player, check)) {return true};
-                j++;
-            }
-            return false;
-        },
-
-
-        */
         checkWinner(board, player, i, j, check) {
             // horizontal
             if (this.checkHorizontal(board, player, i, j, check)) {return true;}
-        
             // vertical
-            //if (this.checkHorizontal(this.transpose(this.copy(board)), player, j, i, check)) {return true;};
-            //if (this.checkHorizontal(_.zip.apply(_, board), player, j, i, check)) { return true;};
             if (this.checkVertical(board, player, i, j, check)) {return true;}
             // diagonal \
             if (this.checkDiagonal(board, 19, player, i, j, check)) {return true;};
-        
+
             // diagonal /
-            //var result = this.checkDiagonal(board.map(function(matrix){return matrix.reverse();}), 19, player, i, j, check);
-            //board.map(function(matrix){return matrix.reverse();})
             return this.checkDiagonal_2(board, 19, player, i, j, check);
         },
         checkVertical(board, player, i, j, check) {
@@ -329,41 +244,24 @@ let app = Vue.createApp({
             if (state == 0) {
                 c = "box ";
             } else if (state == 1) {
-                c = "box2 ";
+                c = "black ";
             } else {
-                c = "box3 ";
+                c = "white ";
             }
 
             return c + "border ";
         },
 
         ai_move(board) {
-            console.log("ai", this.running, this.move_n);
-            //sleep(3000);
-            //sleep(1000);
-            //setTimeout(() => console.log("你好!"), 5000)
-            
-            // randomly placing a stone on the board
-            /*
-            var i = Math.round(Math.random() * 18);
-            var j = Math.round(Math.random() * 18);
-
-            while (this.board[i][j] != 0) {
-                i = Math.round(Math.random() * 18);
-                j = Math.round(Math.random() * 18);
-            }
-            this.board[i][j] = -1;
-            */
-
             var root = this.mcst(this.copy(board)); // get monte carlo search tree
             var move = this.get_move(root);
-            this.board[Math.floor(move / 19)][move % 19] = -1;
 
+            this.board[Math.floor(move / 19)][move % 19] = -1;
             this.prev_ai_move[0] = Math.floor(move / 19);
             this.prev_ai_move[1] = move % 19;
-
             this.prev_move[0] = Math.floor(move / 19);
             this.prev_move[1] = move % 19;
+
             this.checkWinner(this.board, -1, Math.floor(move / 19), move % 19, false);
             
         },
@@ -371,18 +269,17 @@ let app = Vue.createApp({
             var max_i = -1;
             var max_val = -1;
             var win_percentage = 0;
-            //console.log("length", root.children.length);
+
             for (var i = 0; i < root.children.length; i++) {
                 var c = root.children[i];
-                console.log(i, Math.floor(c.move / 19), c.move % 19, c.numerator, c.denominator, c.numerator/ c.denominator);
-                //console.log(Math.floor(c.move / 19), c.move % 19);
+                //console.log(i, Math.floor(c.move / 19), c.move % 19, c.numerator, c.denominator, c.numerator/ c.denominator);
                 if (c.denominator > max_val) {
                     max_val = c.denominator;
                     win_percentage = c.numerator / c.denominator;
                     max_i = i;
                 }
             }
-            console.log("max", Math.floor(root.children[max_i].move / 19), root.children[max_i].move % 19);
+            //console.log("max", Math.floor(root.children[max_i].move / 19), root.children[max_i].move % 19);
             if (root.children[max_i].denominator != 1 || root.children[max_i].numerator != 1) {
                 this.win_prob = (100 - win_percentage * 100).toFixed(2);
             }
@@ -395,28 +292,11 @@ let app = Vue.createApp({
             }
             return false;
         },
-        /*
-        get_move(root) {
-            var max_i = -1;
-            var max_val = -1;
-            console.log("length", root.children.length);
-            for (var i = 0; i < root.children.length; i++) {
-                var c = root.children[i];
-                console.log(i, c.numerator, c.denominator);
-                if (c.numerator / c.denominator > max_val) {
-                    max_val = c.numerator / c.denominator;
-                    max_i = i;
-                }
-            }
-            return root.children[max_i].move;
-        },*/
-
 
         mcst(board) {
             var root = new Node();
             var available = this.get_available(board);
             
-            var actions_found = false;
             // find ai consecutive 4
             for (var i = 0; i < available.length; i++) {
                 var b = this.copy(board);
@@ -457,10 +337,8 @@ let app = Vue.createApp({
                 var move = available[i];
                 b[Math.floor(move / 19)][move % 19] = -1;
                 var lst = this.live_4(Math.floor(move / 19), move % 19, -1, b);
-                //console.log("lst", lst);
                 live = live.concat(lst);
             }
-            //console.log(live);
             if (live.length != 0) {
                 console.log("ai live 4 next");
                 for (var i = 0; i < live.length; i++) {
@@ -470,23 +348,6 @@ let app = Vue.createApp({
                 }
             }
 
-            /*
-            // find human live 3
-            var live_opponent = this.live_3(1);
-            if (live_opponent.length != 0) {
-                for (var i = 0; i < live_opponent.length; i++) {
-                    let node = new Node();
-                    node.move = live_opponent[i];
-                    root.children.push(node);
-                }
-            } else {
-                // find all moves within the same block as existing stones
-                for (var i = 0; i < available.length; i++) {
-                    let node = new Node();
-                    node.move = available[i];
-                    root.children.push(node);
-                }
-            }*/
             var live_opponent = [];
             // find human moves that will form a live 4 in the next turn
             for(var i = 0; i < available.length; i++) {
@@ -512,9 +373,6 @@ let app = Vue.createApp({
                 }
             }
 
-
-            
-
             for (var i = 0; i < 30000; i++) {
                 let update = this.traverse(this.copy(board), root, 1);
                 root.numerator += update;
@@ -531,7 +389,6 @@ let app = Vue.createApp({
             if (root.children.length == 0) {
                 if (root.denominator != 0) {
                     var available = this.get_available(board);
-                    //console.log("available", available.length);
                     for (var i = 0; i < available.length; i++) {
                         let node = new Node();
                         node.move = available[i];
@@ -541,11 +398,9 @@ let app = Vue.createApp({
                 }
                 // simulate and return backpropogate results
                 let result = this.simulate(this.copy(board), player);
-                //console.log("result", + result);
-                //console.log(root, depth);
                 root.numerator += +result;
                 root.denominator += 1;
-                //console.log("after",root, depth);
+
                 return result;
             }
         
@@ -585,20 +440,6 @@ let app = Vue.createApp({
 
 
         live_4(i, j, player, board) {
-            /*
-            var i = -1;
-            var j = -1;
-            if (player == 1) {
-                i = this.prev_human_move[0];
-                j = this.prev_human_move[1];
-            } else {
-                i = this.prev_ai_move[0];
-                j = this.prev_ai_move[1];
-            }
-            var available = [];
-            if (i == -1) {
-                return available;
-            }*/
             var available = [];
             
             //horizontal
@@ -704,28 +545,9 @@ let app = Vue.createApp({
                 }
             }
             return available;
-            /*
-            var list = board.join(",").split(',').map(Number);
-            var available = [];
-            var range = 1;
-            for (var i = 0; i < list.length; i++) {
-                if (list[i] != 0) {
-                    var row = Math.floor(i / 19);
-                    var col = i % 19;
-                    for (var j = Math.max(row - range, 0); j <= Math.min(row + range, list.length - 1); j++) {
-                        for (var k = Math.max(col - range, 0); k <= Math.min(col + range, list.length - 1); k++) {
-                            if (list[j * 19 + k] == 0 && available.indexOf(j * 19 + k) == -1) {
-                                available.push(j * 19 + k);
-                            }
-                        }
-                    }
-                }
-            }
-            return available;*/
         },
 
         get_all_available(board) {
-            //var list = board.join(",").split(',').map(Number);
             var available = [];
             for (var i = 0; i < 19; i++) {
                 for (var j = 0; j < 19; j++) {
@@ -744,62 +566,32 @@ let app = Vue.createApp({
             var available = this.get_all_available(board);
         
             var player = p;
-            //console.log(player);
             // randomly place stones until someone wins
             while (true) {
                 // randomly sample move
                 if (available.length == 0) {
-                    //print_board(board);
                     return 0.5;
                 }
                 var idx = Math.round(Math.random() * (available.length - 1));
-                //console.log(idx, available.length);
                 var move = available[idx];
-                //available.splice(idx, 1);
                 var temp = available[idx];
+
+                // remove move from available
                 available[idx] = available[available.length - 1];
                 available[available.length - 1] = temp;
                 available.pop();
-                //console.log(available.length, idx, move, player);
-                //console.log(move);
+
                 var current_i = Math.floor(move / 19);
                 var current_j = move % 19;
                 board[current_i][current_j] = player;
+                // check if the player at this turn has won
                 if (this.checkWinner(board, player, current_i, current_j, true)) {
-                    //console.log(i)
                     return (player == -1);
                 }
                 player = -player;
-                //print_board(board)
             }
-        
-            //console.log(checkWinner(board, player));
-            //print_board(board);
-            return (player == -1);
         },
-        /*
-        simulate(board, p) {
-            // get all availables moves
-            var available = this.get_all_available(board);
-            
 
-            var player = p;
-            // randomly place stones until someone wins
-            while (!this.checkWinner(board, -player)) {
-                if (available.length == 0) {
-                    //print_board(board);
-                    return 0.5;
-                }
-                // randomly sample move
-                var idx = Math.round(Math.random() * (available.length - 1));
-                var move = available[idx];
-                available.splice(idx, 1);
-                board[Math.floor(move / 19)][move % 19] = player;
-                player = -player;
-            }
-
-            return (player == -1);
-        },*/
         copy(board) {
             var b = new Array(19);
             for (var i = 0; i < 19; i++) {
@@ -812,14 +604,5 @@ let app = Vue.createApp({
 
     }
 })
-//app.start_game();
 
 app.mount('#app')
-
-function sleep(delay) {
-    var start = (new Date()).getTime();
-    while ((new Date()).getTime() - start < delay) {
-        // 使用  continue 实现；
-        continue; 
-    }
-}
